21fff5ab2ad832a2f2f5038f98ce29b26019b8c1
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-01-19 12:48:45 +0100

    fix

--- R/batch.R
+++ R/batch.R
@@ -11,3 +11,21 @@ setupBatches <- function(vec, nBatches) {
       rep(nBatchesLarge+seq_len(nBatchesSmall), each=batchSizeSmall)))
   return(batches)
 }
+
+
+#' @export
+splitAndGetOneBatch <- function(name, values, nBatches, batchIndex) {
+  if (!hasValue(nBatches) || nBatches[[1]] == 1) {
+    cat("All", name, "values are one batch.\n")
+    return(values)
+  }
+  cat("Split", name, "into", nBatches, "batches.\n")
+  batches <- setupBatches(values, nBatches)
+  batch <- batches[[batchIndex]]
+  if (length(batch) == 0) {
+    cat("Batch", batchIndex, "is empty. Nothing to do.\n")
+    return(invisible())
+  }
+  cat("Process batch", batchIndex, "with", length(batch), name, "values.\n")
+  return(batch)
+}
