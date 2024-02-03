70b45f99583de5889c6c06df719f4166ec4b8dea
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-02-03 12:12:10 +0100

    fix

--- R/path.R
+++ R/path.R
@@ -1,7 +1,10 @@
 #' @export
 makeDirsIfNecessary <- function(path) {
   dirPath <- path |> dirname()
+  fileName <- path |> basename()
   if (!dir.exists(dirPath)) {
+    cat("Create directory", dirPath, "for file", fileName, ".\n")
     dir.create(dirPath, recursive=TRUE)
   }
+  return(file.path(normalizePath(dirPath), fileName))
 }
