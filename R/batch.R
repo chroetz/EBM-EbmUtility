5dab3157610d38e3fb720ef42c312c505480ad33
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-02-10 10:02:19 +0100

    undo path; batch out of bounds

--- R/batch.R
+++ R/batch.R
@@ -21,6 +21,10 @@ splitAndGetOneBatch <- function(name, values, nBatches, batchIndex) {
   }
   cat("Split", name, "into", nBatches, "batches.\n")
   batches <- setupBatches(values, nBatches)
+  if (batchIndex > length(batches)) {
+    cat("Batch", batchIndex, "is out of range. Nothing to do.\n")
+    return(invisible())
+  }
   batch <- batches[[batchIndex]]
   if (length(batch) == 0) {
     cat("Batch", batchIndex, "is empty. Nothing to do.\n")
--- R/path.R
+++ R/path.R
@@ -1,10 +1,9 @@
 #' @export
 makeDirsIfNecessary <- function(path) {
   dirPath <- path |> dirname()
-  fileName <- path |> basename()
   if (!dir.exists(dirPath)) {
     cat("Create directory", dirPath, "for file", fileName, ".\n")
     dir.create(dirPath, recursive=TRUE)
   }
-  return(file.path(normalizePath(dirPath), fileName))
+  return(invisible())
 }
