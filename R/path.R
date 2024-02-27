7eeb1bbb8dd06f3fb0abbef3a809eb7c2fdd23bf
Christof Schoetz <cschoetz@pik-potsdam.de>
2024-02-27 10:01:14 +0100

    fix

--- R/path.R
+++ R/path.R
@@ -2,7 +2,7 @@
 makeDirsIfNecessary <- function(path) {
   dirPath <- path |> dirname()
   if (!dir.exists(dirPath)) {
-    cat("Create directory", dirPath, "for file", fileName, ".\n")
+    cat("Create directory", dirPath, "for file", path, ".\n")
     dir.create(dirPath, recursive=TRUE)
   }
   return(invisible())
